f99b209e3699c4b8a42258f1acb2db4aabc1960a
yupenglei <yupengniaosanshi@gmail.com>
2018-11-08 00:21:37 +0800

    添加了一些注释

--- hello.scm
+++ hello.scm
@@ -52,24 +52,29 @@
 
 ;定义表结构
 (defstruct table (equ eqv?) (alist '()))
+;;;从table中查找k的值，默认为d的car，比较器在table中已定义
 (define table-get
   (lambda (tbl k . d)
-	(let ((c (lassoc k (table.alist tbl)  (table.equ tbl))))
-	  (cond (c (cdr c))
-			((pair? d)  (car d))))))
+	(let ((c (lassoc k (table.alist tbl)  (table.equ tbl))));定义c为tbl中key为k的pair
+	  (cond (c (cdr c));若c存在，则返回cdr即value
+			((pair? d)  (car d))))));c不存在，且默认值d为pair则返回d的car
+;;;从列表al中查找k对应的value，比较器为参数equ?
 (define lassoc
-  (lambda (k al equ?)
+  (lambda (k al equ?);从pairList的al中查找key为k的pair，没有则返回#f
 	(let loop ((al al))
 	  (if (null? al) #f
 		(let ((c (car al)))
 		  (if (equ? (car c) k) c
 			(loop (cdr al))))))))
+;;;向table中指定的key赋值value
 (define table-put!
   (lambda (tbl k v)
 	(let ((al (table.alist tbl)))
 	  (let ((c (lassoc k al (table.equ tbl))))
-		(if c (set-cdr! c v)
-		  (set!table.alist tbl (cons (cons k v) al)))))))
+		(if c (set-cdr! c v);若tbl中已存在key为k的pair，则更新其value
+		  (set!table.alist tbl (cons (cons k v) al)))))));否则将tbl更新为开头插入新pair的list
+;;;该函数接受一个table和一个两参数的过程p，
+;;;然后遍历table每一个pair，对pair应用过程p
 (define table-for-each
   (lambda (tbl p)
 	(for-each
@@ -79,6 +84,22 @@
 
 ;;; todo 未运行成功
 (define foo '((a . 1) (b . 2) (c . 3)))
+
+(for-each myPrint foo)
+
+(table-put! foo 'a 111)
+
+(table.alist foo)
+
+(table.equ foo)
+
+(define myPrint 
+  (lambda (x)
+	(display (car x))
+	(display '-)
+	(display (cdr x))
+	(newline)))
+
 (table-get foo 'b )
 
-foo
+;todo 知道问题在哪了；defstruct中创建的是vector，这里当做list处理了，明天再改
